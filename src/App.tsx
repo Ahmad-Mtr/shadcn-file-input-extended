8e9bce61d3d049a8c4b2c094e7c6a2347159aa88
vikramsamak <vikramsamak02@gmail.com>
2025-02-16 19:24:40 +0530

    add FileInputCard component and integrate it into the main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { FileInputCard } from "./components/file-input-card";
+
 function App() {
-  return <main></main>;
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <FileInputCard />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/file-input-card/index.ts
@@ -0,0 +1,1 @@
+export { default as FileInputCard } from "./file-input-card";
