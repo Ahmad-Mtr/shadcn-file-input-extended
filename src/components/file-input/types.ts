fc175f70f211747bf2453846f27e2a16f3fa1b98
vikramsamak <vikramsamak02@gmail.com>
2025-02-16 20:58:28 +0530

    enhance FileInput types with ControllerRenderProps and add FileList and FileItem interfaces

--- src/components/file-input/types.ts
+++ src/components/file-input/types.ts
@@ -1,4 +1,18 @@
+import { ControllerRenderProps } from "react-hook-form";
+
 export type FileInputProps = {
-  multiple?: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  field: ControllerRenderProps<any, any>;
   accept?: string;
+  multiple?: boolean;
 };
+
+export interface FileListProps {
+  files: File[];
+  onRemove: (index: number) => void;
+}
+
+export interface FileItemProps {
+  file: File;
+  onRemove: () => void;
+}
