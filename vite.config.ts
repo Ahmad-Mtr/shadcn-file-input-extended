cce4a2169def12e264bf4a0eb4a09e3b5fa3c82a
vikramsamak <vikramsamak02@gmail.com>
2025-02-23 20:52:40 +0530

    refactor: simplify Vite configuration by removing unused dts plugin and updating path alias to 'registry'

--- vite.config.ts
+++ vite.config.ts
@@ -2,60 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      include: ['lib/**/*.ts', 'lib/**/*.tsx'],
-      insertTypesEntry: true,
-      outDir: 'dist',
-      copyDtsFiles: true,
-      rollupTypes: true,
-      tsconfigPath: './tsconfig.app.json',
-    }),
-    tailwindcss(),
-  ],
-  build: {
-    minify: 'esbuild',
-    sourcemap: true,
-    lib: {
-      entry: path.resolve(__dirname, 'lib/index.ts'),
-      name: 'shadcn-file-input',
-      fileName: (format) => `shadcn-file-input.${format}.js`,
-    },
-    rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        'clsx',
-        'tailwind-merge',
-        'react-hook-form',
-        '@radix-ui/react-slot',
-        'lucide-react',
-        '@tailwindcss/vite',
-        'tailwindcss-animate',
-      ],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          clsx: 'clsx',
-          'tailwind-merge': 'tailwindMerge',
-          'react-hook-form': 'ReactHookForm',
-          '@radix-ui/react-slot': 'RadixSlot',
-          'lucide-react': 'LucideReact',
-          '@tailwindcss/vite': 'TailwindVite',
-          'tailwindcss-animate': 'TailwindAnimate',
-        },
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './lib'),
+      '@': path.resolve(__dirname, './registry'),
     },
   },
 });
