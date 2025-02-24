1da718298848192ac4113ce94356e50e88f06e50
vikramsamak <vikramsamak02@gmail.com>
2025-02-24 20:37:45 +0530

    feat: add header and theme provider components with mode toggle functionality

--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -1,40 +1,44 @@
 "use client";
-import { Code } from "@/components/Code";
+import { Code } from "@/components/code";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useState } from "react";
+import Header from "@/components/header";
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState("preview");
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center p-6 bg-gray-100">
-      <Card className="w-full max-w-2xl">
-        <CardHeader>
-          <CardTitle>ShadCN File Input</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <Tabs value={activeTab} onValueChange={setActiveTab}>
-            <TabsList className="flex gap-2 justify-start">
-              <TabsTrigger value="preview">Preview</TabsTrigger>
-              <TabsTrigger value="code">Code</TabsTrigger>
-            </TabsList>
-            <TabsContent value="preview">
-              <p>FileInputForm</p>
-            </TabsContent>
-            <TabsContent value="code">
-              <Code
-                code={`
+    <>
+      <Header />
+      <main className="min-h-screen flex flex-col items-center justify-center p-6">
+        <Card className="w-full max-w-2xl">
+          <CardHeader>
+            <CardTitle>ShadCN File Input</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Tabs value={activeTab} onValueChange={setActiveTab}>
+              <TabsList className="flex gap-2 justify-start">
+                <TabsTrigger value="preview">Preview</TabsTrigger>
+                <TabsTrigger value="code">Code</TabsTrigger>
+              </TabsList>
+              <TabsContent value="preview">
+                <p>FileInputForm</p>
+              </TabsContent>
+              <TabsContent value="code">
+                <Code
+                  code={`
 \`\`\`ts {4} showLineNumbers
 const add = (a: number, b: number): number => a + b;
 \`\`\`
 `}
-              />
-            </TabsContent>
-          </Tabs>
-        </CardContent>
-      </Card>
-      <Button className="mt-6">View on GitHub</Button>
-    </main>
+                />
+              </TabsContent>
+            </Tabs>
+          </CardContent>
+        </Card>
+        <Button className="mt-6">View on GitHub</Button>
+      </main>
+    </>
   );
 }
---
+++ example/src/components/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ModeToggle } from "./mode-toggle";
+
+function Header() {
+  return (
+    <header>
+      <ModeToggle />
+    </header>
+  );
+}
+
+export default Header;
